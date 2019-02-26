71684b78cabba3661d2fe5b6916765faf682da33
Joey Cho <cjy12182000@naver.com>
2019-02-26 10:28:36 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,9 +17,8 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = Post.new(author_id: params[:author_id])
   end
-
   def create
     @post = Post.new(post_params)
     @post.save
